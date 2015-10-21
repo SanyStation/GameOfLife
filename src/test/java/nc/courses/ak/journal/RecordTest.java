f476407e691c3a8390204d492a7da420b51c9043
olko06141 <sanystation@gmail.com>
2015-10-21 14:32:15 +0300

    Fixed messages

--- src/test/java/nc/courses/ak/journal/RecordTest.java
+++ src/test/java/nc/courses/ak/journal/RecordTest.java
@@ -27,27 +27,27 @@ public class RecordTest {
     @Test(expected = IllegalEventFormatJournalException.class)
     public void testTimeParsing() throws IllegalEventFormatJournalException {
         System.out.println("Time_Parsing");
-        Record r = new Record("2013-12-01 18:3040 !!!!! Ukraine Revolution !");
+        Record r = new Record("2013-12-01 18:3040 !!!!! Message !");
     }
 
     @Test(expected = IllegalEventFormatJournalException.class)
     public void testDateParsing() throws IllegalEventFormatJournalException {
         System.out.println("Date_Parsing");
-        Record r = new Record("01a3-2-01 18:30:40 !!!!! Ukraine Revolution !");
+        Record r = new Record("01a3-2-01 18:30:40 !!!!! Message !");
     }
 
     @Test(expected = IllegalEventFormatJournalException.class)
     public void testImportanceParsing()
             throws IllegalEventFormatJournalException {
         System.out.println("Importance_Parsing");
-        Record r = new Record("2013-12-01 18:30:40 !!!+ Ukraine Revolution !");
+        Record r = new Record("2013-12-01 18:30:40 !!!+ Message !");
     }
 
     @Test
     public void testEventParsing()
             throws IllegalEventFormatJournalException {
         System.out.println("Event_Parsing");
-        Record r = new Record("2013-12-01 18:30:40 !!!!! Ukraine Revolution !");
+        Record r = new Record("2013-12-01 18:30:40 !!!!! Message!");
     }
 
 }
